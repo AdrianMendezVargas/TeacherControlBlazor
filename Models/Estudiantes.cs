853c6018e56b428f749d4768caebec2984d723b7
Enel Almonte <enelramon@hotmail.com>
2020-05-06 10:11:22 -0400

    Agregando registro de Estudiantes

---
+++ Models/Estudiantes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeacherControl.Models
+{
+    public class Estudiantes
+    {
+        [Key]
+        public int EstudianteId { get; set; }
+
+        [Required(ErrorMessage ="Es obligatorio introducir el nombre")]
+        public string Nombres { get; set; }
+
+        [Range(minimum:1, maximum:10,ErrorMessage ="Seleccione un semestre")]
+        public int Semestre { get; set; }
+
+        public int NacionalidadId { get; set; }
+
+        [ForeignKey("NacionalidadId")]
+        public virtual Nacionalidades Nacionalidad { get; set; }
+    }
+}
---
+++ Models/Nacionalidades.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeacherControl.Models
+{
+    public class Nacionalidades
+    {
+        [Key]
+        public int NacionalidadId { get; set; }
+        public string Nacionalidad { get; set; }
+    }
+}
