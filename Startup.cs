b3c1322bb086e9cffc874cdce4a89f695b66600b
Enel Almonte <enelramon@hotmail.com>
2020-05-17 00:44:22 -0400

    Completando registro de estudiantes

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazored.Toast;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,6 +29,8 @@ namespace TeacherControl
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddBlazoredToast();
+
             services.AddSingleton<WeatherForecastService>();
         }
 
